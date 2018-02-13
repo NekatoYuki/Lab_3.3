63bcd32973b28c6d8f551265a224fe5030524d9d
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2018-02-13 08:57:13 -0500

    asdf

---
+++ Lab_3.3_Final_Sorts/src/FinalSorts.java
@@ -0,0 +1,27 @@
+
+public class FinalSorts {
+
+		public static void main(String[] args)
+		{
+			
+			int [] list1 = {1,2,3,4,5,6,7,8,9};
+			
+		}
+		
+		public static String [] mergeSort(String [] list1)
+		{
+		
+		return list1;
+		}
+		
+		public static int partition(int [] list1, int front, int back)
+		{
+			int pivot = 0;
+		return pivot;
+		}
+		
+		public static void quickSort(int [] list1, int front, int back)
+		{
+	
+		}
+}
