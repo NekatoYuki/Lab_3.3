eda2f167122c70c30bb7dfb49049a1f892748b97
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2018-02-13 09:10:32 -0500

    asdf

--- Lab_3.3_Final_Sorts/src/FinalSorts.java
+++ Lab_3.3_Final_Sorts/src/FinalSorts.java
@@ -16,8 +16,23 @@ public class FinalSorts {
 		
 		public static int partition(int [] list1, int front, int back)
 		{
-			int pivot = 0;
-		return pivot;
+			int pivot = list1[front]; //set pivot as front of list1
+			int x = front -1; // x = front pivot -1
+			int y = back +1; //y = back pivot +1
+			while ( x < y) //for as long as the the two pivot points haven't crossed
+			{
+				for (x++; list1[x] < pivot; x++); //increment x by 1
+				for (y--; list1[y] > pivot; y--); //increment y by 1
+				{
+					if (x < y) //if x is less than y
+					{
+						int temp = list1[x]; //swap method
+						list1[x] = list1[y];
+						list1[y] = temp;
+					}
+				}
+			}
+			return y;
 		}
 		
 		public static void quickSort(int [] list1, int front, int back)
