1a9863f135825a4b3376e65d2fa50b1c6a521748
BT_1N3_24 <BT_1N3_24@BT_1N3_24.bktech.local>
2018-02-14 09:23:18 -0500

    asdf

--- Lab_3.3_Final_Sorts/src/FinalSorts.java
+++ Lab_3.3_Final_Sorts/src/FinalSorts.java
@@ -22,13 +22,9 @@ public class FinalSorts {
 		
 		public static int partition(int [] list1, int front, int back)
 		{
-			int pivot = list1[front]; //set pivot as front of list1
+			int pivot = list1[back]; //set pivot as front of list1
 			int x = front -1; // x = front pivot -1
-			int y = back +1; //y = back pivot +1
-			while ( x < y) //for as long as the the two pivot points haven't crossed
-			{
-				for (x++; list1[x] < pivot; x++); //increment x by 1
-				for (y--; list1[y] > pivot; y--); //increment y by 1
+			for (int y = 0; list1[y] > pivot; y--); //increment y by 1
 				{
 					if (x < y) //if x is less than y
 					{
